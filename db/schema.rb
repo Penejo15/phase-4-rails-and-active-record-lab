2f609ee291f675910b55efe298672afa9f431f96
Peter <Petstorm101@gmail.com>
2022-10-16 17:51:31 +0100

    rails and active record lab

---
+++ db/schema.rb
@@ -0,0 +1,11 @@
+ActiveRecord::Schema.define(version: 2021_04_30_181635) do
+
+    create_table "students", force: :cascade do |t|
+      t.string "first_name"
+      t.string "last_name"
+      t.integer "grade"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+    end
+  
+end
